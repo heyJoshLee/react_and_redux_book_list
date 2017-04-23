0ab1ee9bcd3be7822f04d32f747fea2b3b346c27
jleewebdev <lee45276@gmail.com>
2017-04-23 15:07:37 +0800

    Initial/final commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: "Title one", pages: 101},
+    {title: "Title two", pages: 39},
+    {title: "Title three", pages: 85},
+    {title: "Title four", page: 1}
+  ]
+}
